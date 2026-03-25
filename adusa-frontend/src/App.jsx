619119abd79f6dcd3f6e96426a7d70ff14925398
ashish8112 <ashish233227shukla@gmail.com>
2026-03-25 19:46:34 +0530

    Added Student Component

--- adusa-frontend/src/App.jsx
+++ adusa-frontend/src/App.jsx
@@ -1,53 +1,15 @@
-function ProfileCard({name,branch,bio})
+import StudentCard from './StudentCard'
+function App()
 {
-    return(
-      <div>
-        <h2>{name}</h2>
-        <p>Branch: {branch}</p>
-        <p>Bio: {bio}</p>
-        <SkillBadge skill={"NodeJs"}/>
-        <SkillBadge skill={"MongoDB"}/>
-        <SkillBadge skill={"ExpressJs"}/>
-      </div>
-    );
-}
-
-function WelcomeBanner({collegeName,tagline})
-{
-  return(
+  return (
     <div>
-      <h2>Welcome in {collegeName}</h2>
-      <h3>Tagline : {tagline}</h3>
-    </div>
-  )
-}
-function SkillBadge({skill}){
-  return(
-    <>
-      <pre>['{skill}']</pre>
-    </>
-  )
-}
-
-function App()  //or write here export default app
-{
-  return(
-    <div>
-      <WelcomeBanner collegeName="Kristu Jayanti University" tagline={"Where Coders Meet"} /> 
-      <ProfileCard name="Ashu" branch="CSE" bio="I love Coding" />
-      <ProfileCard name ="Rahul" branch="ECE" bio="I love businnes"/>
-      <ProfileCard name ="Vikas" branch="CSE" bio="I love gym"/>
+      <h1>Welcome to Adusa</h1>
+      <StudentCard name="Ashu" college="Kristu Jayanti" branch="MCA" />
+      <br></br>
+      <StudentCard name="Amrita" college="IIM" branch="MBA" />
+      <h2>To Run on mobile use npm run dev -- --host. It will expose to ip not only on localhost</h2>
     </div>
   )
 }
 
 export default App;
-
-//Note in props we use {} to send object or array or number for string we can use directly ""
-//for object -> {{key:value}} outside specify sending other than string inside object is written
-
- // Tumne likha:
-// skill={"NodeJs"}   // kaam karta hai but unnecessary
-
- // Sahi:
-// skill="NodeJs"     // string = seedha quotes ✅
