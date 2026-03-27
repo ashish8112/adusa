b5f039095cbd4f32a6c48e7044012754aceb4520
ashish8112 <ashish233227shukla@gmail.com>
2026-03-28 00:55:53 +0530

    Add bio prop to StudentCard component and update usage in App

--- adusa-frontend/src/App.jsx
+++ adusa-frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App()
       <h1>Welcome to Adusa</h1>
       <StudentCard name="Ashu" college="Kristu Jayanti" branch="MCA" />
       <br></br>
-      <StudentCard name="Amrita" college="IIM" branch="MBA" />
+      <StudentCard name="Amrita" college="IIM" branch="MBA" bio="Radhey Radhey" />
       <h2>To Run on mobile use npm run dev -- --host. It will expose to ip not only on localhost</h2>
     </div>
   )
--- adusa-frontend/src/StudentCard.jsx
+++ adusa-frontend/src/StudentCard.jsx
@@ -1,10 +1,11 @@
-function StudentCard({name,college,branch})
+function StudentCard({name,college,branch,bio="No bio Yet"})
 {
     return (
         <div>
             <h2>{name}</h2>
             <p>College: {college}</p>
             <p>Branch : {branch}</p>
+            <p>Bio :{bio}</p>
         </div>
     );
 }
