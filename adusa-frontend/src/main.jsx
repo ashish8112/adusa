220112de224278932be137fea5b4898c480a694d
unknown <ashish233227shukla@gmail.com>
2026-05-21 16:41:59 +0530

    Added Comment of code

--- adusa-frontend/src/main.jsx
+++ adusa-frontend/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client' // named import for react dom to create react dom 
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' //default import
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />) // creating root node where we can define structure and layout of our app
