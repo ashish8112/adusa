0c6047425bca53691befb81b6d6c2ab9239fef64
unknown <ashish233227shukla@gmail.com>
2026-05-22 22:31:08 +0530

    Updated UserFetch.jsx

--- adusa-frontend/src/component/UserFetch.jsx
+++ adusa-frontend/src/component/UserFetch.jsx
@@ -1,20 +1,18 @@
-import { useEffect,useState } from "react";
+import {useState , useEffect} from "react"
 
-useEffect(() => {
-  async function getUsers() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users")
-    const data = await res.json()
-    setUsers(data)
-  }
-  getUsers()
-}, [])
-
-     return(
-        <div>
-            {users.map((user)=>(
-                <p key={user.id}>{user.name}</p>
-            ))}
-        </div>
-     )
-
-
+export default function UserFetch()
+{
+  const [users,setUsers]=useState([])
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+  },[])
+  return(
+    <div>
+      {users.map((user)=>{
+        <p key={user.id}>{user.name}</p>
+      })}
+    </div>
+  )
+}
