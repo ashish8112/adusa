ce7b249e4d0714eb132c038298aa0ad32aacab7d
Ashish <ashish233227shukla@gmail.com>
2026-05-23 17:38:31 +0530

    Updated

--- adusa-frontend/src/component/UserFetch.jsx
+++ adusa-frontend/src/component/UserFetch.jsx
@@ -1,15 +1,30 @@
-import {useState , useEffect} from "react"
+import {useState , useEffect, use} from "react"
 
 export default function UserFetch()
 {
   const [users,setUsers]=useState([])
+  const [loading,setLoading]=useState(true)
+  const [error,setError]=useState(null)
   useEffect(()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res=>res.json())
     .then(data=>{
       console.log(data)
-      setUsers(data)})
+      setUsers(data)
+      setLoading(false)
+    })
+    .catch((err)=>{
+      setError("Something went wrong")
+      setLoading(false)
+    })
   },[])
+
+
+
+  if(loading)
+    return <p> {"loading ...."}</p>
+  if(error)
+    return <p>{error}</p>
   return(
     <div>
       {users.map((user)=>
