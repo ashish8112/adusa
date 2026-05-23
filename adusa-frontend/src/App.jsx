5e31fbbadcf4369987eeb5b72b35aa5c80dea67c
unknown <ashish233227shukla@gmail.com>
2026-05-23 16:31:28 +0530

    Updated Src

--- adusa-frontend/src/App.jsx
+++ adusa-frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import StudentCard from './component/StudentCard'
+import UserFetch from './component/UserFetch';
 function App()
 {
-  return (
-    <div className='container'>
-      <h1>Welcome to Adusa</h1>
-      <StudentCard name="Ashu" college="Kristu Jayanti" branch="MCA" />
-      <br></br>
-      <StudentCard name="Amrita" college="IIM" branch="MBA" bio="Radhey Radhey" />
-      <h2>To Run on mobile use npm run dev -- --host. It will expose to ip not only on localhost</h2>
-    </div>
+  // return (
+  //   <div className='container'>
+  //     <h1>Welcome to Adusa</h1>
+  //     <StudentCard name="Ashu" college="Kristu Jayanti" branch="MCA" />
+  //     <br></br>
+  //     <StudentCard name="Amrita" college="IIM" branch="MBA" bio="Radhey Radhey" />
+  //     <h2>To Run on mobile use npm run dev -- --host. It will expose to ip not only on localhost</h2>
+  //   </div>
+  // )
+  return(
+    <UserFetch />
   )
 }
 
