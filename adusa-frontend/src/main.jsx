5bcc6af206f29863ce06b3273b287ffd78ad3cff
Ashish <ashish233227shukla@gmail.com>
2026-05-31 15:46:54 +0530

    Added Browser router

--- adusa-frontend/src/main.jsx
+++ adusa-frontend/src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client' // named import for react dom to create react dom 
 import './index.css'
 import App from './App.jsx' //default import
+import {BrowserRouter} from "react-router-dom"
 
-createRoot(document.getElementById('root')).render(<App />) // creating root node where we can define structure and layout of our app
+createRoot(document.getElementById('root'))
+.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+) // creating root node where we can define structure and layout of our app
