c73df4b01f33927c8bacb6b43862993dd9a1d234
Ashish <ashish233227shukla@gmail.com>
2026-06-01 23:44:58 +0530

    Updated Profile Page

--- adusa-frontend/src/pages/Profile.jsx
+++ adusa-frontend/src/pages/Profile.jsx
@@ -1,6 +1,12 @@
+import {useNavigate} from "react-router-dom"
 export default function Profile()
 {
+    const navigate = useNavigate();
     return (
-        <h1>Profile Page</h1>
+        <div>
+            <h1>Profile Page</h1>
+            <button onClick={()=>navigate("/")}>Logout</button>
+        </div>
+        
     )
 }
