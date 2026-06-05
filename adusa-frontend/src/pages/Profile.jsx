4de515f1eeb725be8640f87e6f2d0c0627660124
Ashish <ashish233227shukla@gmail.com>
2026-06-05 14:56:30 +0530

    Added useParam hooks for dynamic routing

--- adusa-frontend/src/pages/Profile.jsx
+++ adusa-frontend/src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import {useNavigate} from "react-router-dom"
+import {useParams} from "react-router-dom"
 export default function Profile()
 {
     const navigate = useNavigate();
+    const {userId}=useParams();
     return (
         <div>
-            <h1>Profile Page</h1>
+            <h1>Profile Page:{userId}</h1>
             <button onClick={()=>{navigate("/")}}>Logout</button>
         </div>     
     )
