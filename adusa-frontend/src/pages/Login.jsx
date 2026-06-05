2a7b375a3e77829396a893c8b548c013c976b8d0
Ashish <ashish233227shukla@gmail.com>
2026-06-05 14:57:14 +0530

    Added Logout Button

--- adusa-frontend/src/pages/Login.jsx
+++ adusa-frontend/src/pages/Login.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom"
 export default function Login()
 {
+    const navigate = useNavigate();
     return(
         <div>
             <h1>Welcome to Login Page</h1>
+            <button onClick={()=>navigate("/")}>Logout</button>
         </div>
         
     )
