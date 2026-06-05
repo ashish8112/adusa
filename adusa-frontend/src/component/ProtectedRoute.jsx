7a03cc92e292afc124f4ef0d2e2a3a95cde83d77
Ashish <ashish233227shukla@gmail.com>
2026-06-05 14:58:43 +0530

    Updated comment of working for Navigate Component

--- adusa-frontend/src/component/ProtectedRoute.jsx
+++ adusa-frontend/src/component/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ function ProtectedRoute({children})
     const token = localStorage.getItem("token")
 
     if(!token)
-        return <Navigate to = "/login" /> // ye line react dekhtey hi turant render karta hain not like useNavigate hook jo eventHandler pe kaam karein 
+        return <Navigate to = "/login" /> // ye line react dekhtey hi turant render karta hain not like useNavigate hook jo eventHandler pe kaam karein and this is the component we need to return 
     
     return children;
 
