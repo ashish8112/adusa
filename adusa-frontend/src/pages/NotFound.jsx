5bf3869c20f810b96cc1421d22794e04c2b6f3fb
Ashish <ashish233227shukla@gmail.com>
2026-06-06 19:34:11 +0530

    Avoid Navigate

--- adusa-frontend/src/pages/NotFound.jsx
+++ adusa-frontend/src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 import {Link} from "react-router-dom"
+import { Navigate } from "react-router-dom"
 export default function NotFound(){
     return(
         <div>
             <h1>404 - Page Not Found </h1>
             <Link to="/">Go to Home</Link>
+            {/* <Navigate to ="/login"/> */} if this is in code it will always automatically redirect to login page without showing above info.
         </div>
     )
 }
