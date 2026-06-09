231109bfcb836af86e34bd4afd39452565e42887
Ashish <ashish233227shukla@gmail.com>
2026-06-09 19:54:46 +0530

    Added Validation

--- adusa-frontend/src/pages/Login.jsx
+++ adusa-frontend/src/pages/Login.jsx
@@ -1,31 +1,45 @@
 import {useState} from "react"
 export default function Login()
 {
-    const[email,setEmail]=useState("");
-    const[password,setPassword]=useState("");
+    const[formData,setFormData]=useState({
+        email:"",
+        password:""
+    });
     function handleSubmit(e){
         e.preventDefault(); {/* Stop Reloading of file*/}
-        console.log({email,password})
+    if(!formData.email||!formData.password)
+    {
+        alert("Please fill all field")
+        return 
+    }
+        console.log(formData)
+    }
+    function handleChange(e){
+        setFormData({...formData,[e.target.name]:e.target.value }) // why square bracket see in day 6 react note
     }
     return(
         <form onSubmit={handleSubmit}>
             <label htmlFor="user-email">Enter the Email</label> {/*not for because reserverd keyword of js for loop */}
             <br/>
+            {/*it will help in e.target.name for becoming key named as email */}
+            {/*name attribute e.target.name mein key ban jaata hai*/}
             <input
                 type="text"
+                name="email"  
                 placeholder="Email"
                 id="user-email"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
+                value={formData.email}
+                onChange={handleChange}
             />
             <br/>
             <label htmlFor="user-password">Enter </label>
             <br/>
             <input
                 type="password"
+                name="password"
                 placeholder="Password"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
+                value={formData.password}
+                onChange={handleChange}
                 id="user-password"
             />
             <br/>
