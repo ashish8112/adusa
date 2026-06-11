3d841aa9a42c491739ad7b9ad7bedf9300ebae39
Ashish <ashish233227shukla@gmail.com>
2026-06-11 10:43:43 +0530

    added component of axios

--- adusa-frontend/src/pages/Home.jsx
+++ adusa-frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link,useNavigate } from "react-router-dom";
-import FetchVsAxios from "../Practice/FetchVsAxios";
+import FetchVsAxios from "../Practice/OldFetchVsAxios";
+import Axios from "../component/Axios";
 function Home(){
   const navigate= useNavigate();
 
@@ -9,7 +10,7 @@ function Home(){
     <Link to ="/login">Go to Login</Link> {/*It is creating clickable link which is appending this string in our url */}
     <br></br>
     <button onClick={()=>navigate("/profile/123")}>Go to Profile</button> {/*Same as above but in button */} 
-    <FetchVsAxios/>
+    <Axios/>
     </div>
   )
   
