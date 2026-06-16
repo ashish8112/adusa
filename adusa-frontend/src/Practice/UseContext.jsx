abc814739a6f01ffd92bc043a685255d04695b35
Ashish <ashish233227shukla@gmail.com>
2026-06-16 09:43:40 +0530

    updated comment in createContext hook

--- adusa-frontend/src/Practice/UseContext.jsx
+++ adusa-frontend/src/Practice/UseContext.jsx
@@ -1,2 +1,4 @@
 import { createContext } from "react";
-export const UserContext = createContext();//Created Context (Global Variable)
+export const UserContext = createContext();//Created Context (Global Variable),createContext return object 
+//UserContext = Context Object (Provider + Consumer) 
+//PascalCase naming Convention, not a component.
