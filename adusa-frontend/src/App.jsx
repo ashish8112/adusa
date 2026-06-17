5675f7157e32932f61c4f990c6704b9863e72d81
Ashish <ashish233227shukla@gmail.com>
2026-06-17 22:20:25 +0530

    added Error Boundary

--- adusa-frontend/src/App.jsx
+++ adusa-frontend/src/App.jsx
@@ -4,16 +4,27 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
 import {Routes,Route} from "react-router-dom"
+import ErrorBoundary from "./practice/ErrorBoundary"
 function App()
 {
   return(
  <Routes>
-  <Route path="/" element={<Home/>} />
-  <Route path="/login" element={<Login/>} />
+  <Route path="/" element={
+    <ErrorBoundary>
+        <Home/>
+    </ErrorBoundary>
+  } />
+  <Route path="/login" element={
+    <ErrorBoundary>
+        <Login/>
+    </ErrorBoundary>
+    } />
   <Route path="/profile/:userId" element={  /*userId act as placeholder anything after profile/:123 will be stored in useParams */
+  <ErrorBoundary>
   <ProtectedRoute>
     <Profile/>
   </ProtectedRoute> 
+  </ErrorBoundary>
   } />
   <Route path="*" element={<NotFound/>}/>
  </Routes>
