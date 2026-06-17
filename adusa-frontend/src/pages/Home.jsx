495da97a3b7199c180297d4ecadd0e06ca8fe586
Ashish <ashish233227shukla@gmail.com>
2026-06-17 20:53:49 +0530

    Added Error Boundary

--- adusa-frontend/src/pages/Home.jsx
+++ adusa-frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { Link,useNavigate } from "react-router-dom";
 import AxiosDemo from "../component/AxiosDemo";
+import ErrorBoundary from "../practice/ErrorBoundary";
+import BuggyComponent from "../practice/BuggyComponent";
 
 function Home(){
   const navigate= useNavigate();
@@ -11,6 +13,9 @@ function Home(){
     <br></br>
     <button onClick={()=>navigate("/profile/123")}>Go to Profile</button> {/*Same as above but in button */} 
     <AxiosDemo/>
+     <ErrorBoundary>
+        <BuggyComponent/>
+    </ErrorBoundary>
     </div>
   )
   
