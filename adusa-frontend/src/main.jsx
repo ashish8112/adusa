67ac5233ce5a9b8ca6c0a5fb870872dcc17215e3
Ashish <ashish233227shukla@gmail.com>
2026-06-17 22:20:48 +0530

    added Error Boundary on Main file

--- adusa-frontend/src/main.jsx
+++ adusa-frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { createRoot } from 'react-dom/client' // named import for react dom to c
 import './index.css'
 import App from './App.jsx' //default import
 import {BrowserRouter} from "react-router-dom"
+import ErrorBoundary from './practice/ErrorBoundary.jsx'
 
 createRoot(document.getElementById('root'))
 .render(
     <BrowserRouter>
-        <App />
+        <ErrorBoundary>
+            <App />
+        </ErrorBoundary>
     </BrowserRouter>
 ) // creating root node where we can define structure and layout of our app
