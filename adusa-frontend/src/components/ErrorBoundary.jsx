2a92876169c91d49549bdd9ebd649245d6095f9c
Ashish <ashish233227shukla@gmail.com>
2026-07-17 19:34:42 +0530

    error boundary class

---
+++ adusa-frontend/src/components/ErrorBoundary.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+export default class ErrorBoundary extends Component{
+    constructor(props){
+        super(props);
+        this.state={hasError:false}
+    }
+    static getDerivedStateFromError(error){
+        return {hasError:true,error:error.message}
+    }
+    componentDidCatch(error,errorInfo){
+        console.error(error,errorInfo);
+    }
+    render(){
+        if(this.state.hasError)
+        {
+            return(
+                <p>Something Went Wrong: {this.state.error}</p>
+            )
+        }
+        return this.props.children
+    }
+}
