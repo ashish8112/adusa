67ece0e9336ca0c9ed3d87c19861ab8e7d861494
Ashish <ashish233227shukla@gmail.com>
2026-07-17 19:34:34 +0530

    added error boundary

--- adusa-frontend/src/main.jsx
+++ adusa-frontend/src/main.jsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client' // named import for react dom to create react dom 
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx' //default import
-
-createRoot(document.getElementById('root')).render(<App />) // creating root node where we can define structure and layout of our app
+import App from './App.jsx' 
+import AuthProvider from './features/auth/AuthProvider.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
+import {BrowserRouter} from "react-router-dom"
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <ErrorBoundary>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </ErrorBoundary>
+    </BrowserRouter>
+) 
