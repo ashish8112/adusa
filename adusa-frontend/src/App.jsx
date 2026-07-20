3844bcd3f8c5918b46c5a8948d128293a22f2c7b
Ashish <ashish233227shukla@gmail.com>
2026-07-20 10:55:00 +0530

    added register page to check working

--- adusa-frontend/src/App.jsx
+++ adusa-frontend/src/App.jsx
@@ -1,7 +1,7 @@
+import Register from "./features/auth/Register";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <Register></Register>
   )
 }
