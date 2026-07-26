0fdbc733cb04a41314c604077596833401302179
Ashish <ashish233227shukla@gmail.com>
2026-07-26 20:22:57 +0530

    added login and register

--- adusa-frontend/src/App.jsx
+++ adusa-frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import Register from "./features/auth/Register";
+import {Routes,Route} from "react-router-dom"
+import ErrorBoundary from "./components/ErrorBoundary"
+import Login from "./features/auth/Login";
 
 export default function App() {
   return (
-   <Register></Register>
+   <Routes>
+    <Route path="/register" element={<ErrorBoundary>
+      <Register></Register>
+    </ErrorBoundary>}/>
+    <Route path="/login" element={<ErrorBoundary>
+      <Login></Login>
+    </ErrorBoundary>}/>
+   </Routes>
   )
 }
