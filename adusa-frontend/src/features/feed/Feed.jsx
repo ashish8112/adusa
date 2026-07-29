5f1037d35bce1778e0cfb36e9ba3cff405969456
Ashish <ashish233227shukla@gmail.com>
2026-07-29 23:21:41 +0530

    dummy data

---
+++ adusa-frontend/src/features/feed/Feed.jsx
@@ -0,0 +1,20 @@
+const posts = [
+  {
+    id: 1,
+    name: "Anjali Kumar",
+    branch: "CSE",
+    content: "Just completed my React project!",
+    likes: 12,
+    comments: 3,
+    time: "2 hours ago"
+  },
+  {
+    id: 2,
+    name: "Rahul Singh",
+    branch: "ECE", 
+    content: "Looking for hackathon teammates!",
+    likes: 8,
+    comments: 5,
+    time: "5 hours ago"
+  }
+]
