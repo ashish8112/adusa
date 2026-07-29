3ef2d07b0a1e2f502254abfe9f597a8d0df44c91
Ashish <ashish233227shukla@gmail.com>
2026-07-29 23:47:32 +0530

    offset pagination

--- adusa-backend/routes/post.js
+++ adusa-backend/routes/post.js
@@ -6,9 +6,9 @@ const verifyToken = require("../middleware/verifyToken")
 router.post("/",verifyToken,async(req,res)=>{
     try{
         let {content} = req.body;
-        if(typeof content!="string")
+        if(typeof content!=="string")
             return res.status(400).json({message:"Content must be text"});
-        content = content?.trim();
+        content = content.trim();
         if(!content)
             return res.status(400).json({message:"Cannot create post with empty content"})
         const post =  new Post({content,author:req.user.id});
@@ -17,5 +17,18 @@ router.post("/",verifyToken,async(req,res)=>{
     }
     catch(err){
         return res.status(500).json({error:err.message})
+    }
+})
+
+router.get("/",async(req,res)=>{
+    try{
+        const page = Number(req.query.page)||1;
+        const limit =5;
+        const skip = (page-1)*limit;
+        const posts = await Post.find({}).sort({createdAt:-1}).skip(skip).limit(limit).populate("author", "name bio");
+        return res.status(200).json(posts);
+    }
+    catch(err){
+        return res.status(500).json({error:err.message});
     }
 })
