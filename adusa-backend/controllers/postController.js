1cc7c649eb7a26dc65ea1cbcd5f6a404fd577dd7
Ashish <ashish233227shukla@gmail.com>
2026-07-31 23:37:36 +0530

    post controller basic

---
+++ adusa-backend/controllers/postController.js
@@ -0,0 +1,34 @@
+const Post = require("../models/Post");
+
+const createPost = async(req,res)=>{
+    try{
+        let {content} = req.body;
+        if(typeof content!=="string")
+            return res.status(400).json({message:"Content must be text"});
+        content = content.trim();
+        if(!content)
+            return res.status(400).json({message:"Cannot create post with empty content"})
+        const post =  new Post({content,author:req.user.id});
+        await post.save();
+        res.status(201).json({message:"Post Created",post});
+    }
+    catch(err){
+        return res.status(500).json({error:err.message})
+    }
+}
+
+const getFeed = async(req,res)=>{
+    try{
+        const page = Number(req.query.page)||1;
+        const limit =5;
+        const skip = (page-1)*limit;
+        const posts = await Post.find({}).sort({createdAt:-1}).skip(skip).limit(limit).populate("author", "name bio");
+        const total = await Post.countDocuments({});
+        return res.status(200).json({posts,page,total,hasMore:skip+posts.length<total});
+    }
+    catch(err){
+        return res.status(500).json({error:err.message});
+    }
+}
+
+module.exports={createPost,getFeed};
