585f31cb6872f93b22ce7ff74493e507602a08f7
Ashish <ashish233227shukla@gmail.com>
2026-08-01 01:07:44 +0530

    handled corrupt data

--- adusa-frontend/src/api/axios.js
+++ adusa-frontend/src/api/axios.js
@@ -2,9 +2,15 @@ import axios from "axios"
 const API = axios.create({baseURL:import.meta.env.VITE_API_URL || "http://localhost:3000/api"})
 
 API.interceptors.request.use((config)=>{
-    const user = JSON.parse(localStorage.getItem("user"));
-    if(user?.token)
+    try{
+        const saved = localStorage.getItem("adusaUser");
+        const user = saved ? JSON.parse(saved):null;
+        if(user?.token)
         config.headers.Authorization = `Bearer ${user.token}`
+    }
+    catch(err){
+        console.error("adusa User is corrupted clear it or check the problem")
+    }
     return config;
 })
 
