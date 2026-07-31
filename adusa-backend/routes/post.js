7091f03c744218618d33a42b76fd38fea5eaea1e
Ashish <ashish233227shukla@gmail.com>
2026-07-31 23:37:05 +0530

    made it modular

--- adusa-backend/routes/post.js
+++ adusa-backend/routes/post.js
@@ -1,38 +1,9 @@
-const Post = require("../models/Post");
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("../middleware/verifyToken");
-const { post } = require("./auth");
+const verifyToken = require("../middleware/verifyToken")
+const {createPost,getFeed} = require("../controllers/postController")
 
-router.post("/",verifyToken,async(req,res)=>{
-    try{
-        let {content} = req.body;
-        if(typeof content!=="string")
-            return res.status(400).json({message:"Content must be text"});
-        content = content.trim();
-        if(!content)
-            return res.status(400).json({message:"Cannot create post with empty content"})
-        const post =  new Post({content,author:req.user.id});
-        await post.save();
-        res.status(201).json({message:"Post Created",post});
-    }
-    catch(err){
-        return res.status(500).json({error:err.message})
-    }
-})
-
-router.get("/",async(req,res)=>{
-    try{
-        const page = Number(req.query.page)||1;
-        const limit =5;
-        const skip = (page-1)*limit;
-        const posts = await Post.find({}).sort({createdAt:-1}).skip(skip).limit(limit).populate("author", "name bio");
-        const total = await Post.countDocuments({});
-        return res.status(200).json({posts,page,total,hasMore:skip+posts.length<total});
-    }
-    catch(err){
-        return res.status(500).json({error:err.message});
-    }
-})
+router.post("/",verifyToken,createPost);
+router.get("/",getFeed);
 
 module.exports=router;
