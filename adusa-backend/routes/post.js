96fd5a2931cd9df9ad61f531bb8f222f1eddc7d8
Ashish <ashish233227shukla@gmail.com>
2026-08-01 00:37:01 +0530

    added toggle like method in routes

--- adusa-backend/routes/post.js
+++ adusa-backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/verifyToken")
-const {createPost,getFeed} = require("../controllers/postController")
+const {createPost,getFeed,toggleLike} = require("../controllers/postController")
 
 router.post("/",verifyToken,createPost);
 router.get("/",getFeed);
+router.post("/:id/like",verifyToken,toggleLike)
 
 module.exports=router;
