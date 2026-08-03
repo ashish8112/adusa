3d75af25c1b5b312ec01ee42923248a570ddd058
Ashish <ashish233227shukla@gmail.com>
2026-08-04 00:33:21 +0530

    added feed for home page

--- adusa-frontend/src/App.jsx
+++ adusa-frontend/src/App.jsx
@@ -2,10 +2,12 @@ import Register from "./features/auth/Register";
 import {Routes,Route} from "react-router-dom"
 import ErrorBoundary from "./components/ErrorBoundary"
 import Login from "./features/auth/Login";
+import Feed from "./features/feed/Feed";
 
 export default function App() {
   return (
    <Routes>
+    <Route path="/" element={<ErrorBoundary><Feed></Feed></ErrorBoundary>}/>
     <Route path="/register" element={<ErrorBoundary>
       <Register></Register>
     </ErrorBoundary>}/>
