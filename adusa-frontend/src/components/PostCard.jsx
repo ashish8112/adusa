b37e10c1b5845f9847a2d9bd73d5a2dc9d6e6f46
Ashish <ashish233227shukla@gmail.com>
2026-08-04 00:33:50 +0530

    changes the color of text

--- adusa-frontend/src/components/PostCard.jsx
+++ adusa-frontend/src/components/PostCard.jsx
@@ -2,7 +2,7 @@ import { getInitials } from "../utils/getInitials"
 import { timeAgo } from "../utils/timeAgo"
 export default function PostCard({post}){
 return(
-    <section className="bg-surface border border-border rounded-lg max-w-96 w-full py-4 px-3 mb-4">
+    <section className="bg-surface border border-border rounded-lg max-w-96 w-full py-4 px-3 mb-4 text-text">
 
         <div className="flex items-center gap-3 mb-3">
             <p>{getInitials(post.author?.name)}</p>
