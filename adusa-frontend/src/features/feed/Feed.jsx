fb0e374051f6341f2fe3abe44bbc9f3278ff6dfe
Ashish <ashish233227shukla@gmail.com>
2026-08-04 00:34:43 +0530

    Created Page(Feed)

--- adusa-frontend/src/features/feed/Feed.jsx
+++ adusa-frontend/src/features/feed/Feed.jsx
@@ -1,20 +1,37 @@
-const posts = [
-  {
-    id: 1,
-    name: "Anjali Kumar",
-    branch: "CSE",
-    content: "Just completed my React project!",
-    likes: 12,
-    comments: 3,
-    time: "2 hours ago"
-  },
-  {
-    id: 2,
-    name: "Rahul Singh",
-    branch: "ECE", 
-    content: "Looking for hackathon teammates!",
-    likes: 8,
-    comments: 5,
-    time: "5 hours ago"
-  }
-]
+import { useState,useEffect } from "react";
+import API from "../../api/axios"
+import PostCard from "../../components/PostCard";
+export default function Feed(){
+  const [loading,setLoading]= useState(true);
+  const [error,setError] = useState(null);
+  const [posts,setPosts]=useState([]);
+  useEffect(()=>{
+    async function fetchPost(){
+      try{
+        const {data} = await API.get("/posts");
+        setPosts(data.posts);
+      }
+      catch(err){
+        setError(err.response?.data?.message||"Failed to get Posts");
+        return ;
+      }
+      finally{
+        setLoading(false);
+      }
+    }
+    fetchPost();
+  },[])
+  if(loading)
+    return(<p>Loading ... wait for few secs </p>);
+  else if(error)
+    return(<p>{error}</p>)
+  else if(posts.length===0)
+    return (<p>No posts</p>)
+  return(
+    <div>
+      {posts.map((post)=>(
+        <PostCard key={post._id} post={post}/>
+      ))}
+    </div>
+  )
+}
