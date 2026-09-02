371ff1f50459b8792168631136a08fa89d9e9dff
Ashish <ashish233227shukla@gmail.com>
2026-09-02 18:00:04 +0530

    docs : updated  suitable comment

--- adusa-backend/controllers/postController.js
+++ adusa-backend/controllers/postController.js
@@ -9,7 +9,7 @@ const createPost = async(req,res)=>{
         if(!content)
             return res.status(400).json({message:"Cannot create post with empty content"})
         const oneHourBefore = new Date(Date.now()-60*60*1000); // new Date() current time in human readable format and Date.now() only number from 1 Jan 1970 midnight perhaps , we are calculating inside for one before and creating Date object for that specific time 
-        const recentPosts = await Post.countDocuments({author:req.user.id,createdAt:{$gte:oneHourBefore}}) //gte = greate than or equall to and mongoDb has BSON Date type which also stores in milliseconds so it requires milliseconds not string like ISO String "2026-08-01T10:00:00Z"
+        const recentPosts = await Post.countDocuments({author:req.user.id,createdAt:{$gte:oneHourBefore}}) //gte = greater than or equal to and mongoDb has BSON Date type which also stores in milliseconds so it requires milliseconds not string like ISO String "2026-08-01T10:00:00Z"
         if(recentPosts>=5) // rate limiting users to post 5 post only in an hour 
             return res.status(429).json({message:"Too many posts. Try again later. "});
         const post =  new Post({content,author:req.user.id});
@@ -26,7 +26,7 @@ const getFeed = async(req,res)=>{
     try{
         const page = Number(req.query.page)||1;
         const limit =5;
-        const skip = (page-1)*limit;
+        const skip = (page-1)*limit;                                                            //note mongoose always send _id unless you write -_id
         const posts = await Post.find({}).sort({createdAt:-1}).skip(skip).limit(limit).populate("author", "name bio").lean(); //lean gives plain js object but with tradeoff not save,populate etc method is not available afterward.
         const postswithLiked = posts.map((post)=>{ //note individual post is plain js now 
                return {...post,liked:req.user?post.likes.some((id)=>(id.toString()===req.user.id)):false} // some will check if it will find true and break when it finds or after end return false if user exists.
