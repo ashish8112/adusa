598b534686a873358928bdc6bf3d8ede586722d7
Ashish <ashish233227shukla@gmail.com>
2026-09-05 11:26:00 +0530

    feat: send avatar along with detail for post

--- adusa-backend/controllers/postController.js
+++ adusa-backend/controllers/postController.js
@@ -27,7 +27,7 @@ const getFeed = async(req,res)=>{
         const page = Number(req.query.page)||1;
         const limit =5;
         const skip = (page-1)*limit;                                                            //note mongoose always send _id unless you write -_id
-        const posts = await Post.find({}).sort({createdAt:-1}).skip(skip).limit(limit).populate("author", "name bio").lean(); //lean gives plain js object but with tradeoff not save,populate etc method is not available afterward.
+        const posts = await Post.find({}).sort({createdAt:-1}).skip(skip).limit(limit).populate("author", "name bio avatar").lean(); //lean gives plain js object but with tradeoff not save,populate etc method is not available afterward.
         const postsWithLiked = posts.map((post)=>{ //note individual post is plain js now 
                return {...post,liked:req.user?post.likes.some((id)=>(id.toString()===req.user.id)):false} // some will check if it will find true and break when it finds or after end return false if user exists.
         })
